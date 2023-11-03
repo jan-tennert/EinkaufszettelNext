19100baa176bf78d6f1e898bfb1fdb598bbe76c7
Jan <jan.m.tennert@gmail.com>
2023-11-03 18:04:13 +0100

    Fix compilation error

--- composeApp/src/commonMain/kotlin/io/github/jan/einkaufszettel/ui/screen/AuthenticatedScreen.kt
+++ composeApp/src/commonMain/kotlin/io/github/jan/einkaufszettel/ui/screen/AuthenticatedScreen.kt
@@ -1,9 +1,11 @@
 package io.github.jan.einkaufszettel.ui.screen
 
+import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.core.screen.Screen
 
 object AuthenticatedScreen: Screen {
 
+    @Composable
     override fun Content() {
 
     }
