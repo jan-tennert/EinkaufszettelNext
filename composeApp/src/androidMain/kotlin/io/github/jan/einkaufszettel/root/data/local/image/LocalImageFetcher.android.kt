2be0ec341fcf5031dfbf58acc44124244a8b1aa6
Jan <jan.m.tennert@gmail.com>
2024-08-16 14:46:18 +0200

    Fix android error

--- composeApp/src/androidMain/kotlin/io/github/jan/einkaufszettel/root/data/local/image/LocalImageFetcher.android.kt
+++ composeApp/src/androidMain/kotlin/io/github/jan/einkaufszettel/root/data/local/image/LocalImageFetcher.android.kt
@@ -3,10 +3,10 @@ package io.github.jan.einkaufszettel.root.data.local.image
 import android.graphics.BitmapFactory
 import coil3.Image
 import coil3.annotation.ExperimentalCoilApi
-import coil3.asCoilImage
+import coil3.asImage
 
 @OptIn(ExperimentalCoilApi::class)
 actual fun createImage(localImageData: LocalImageData): Image {
     val bitmap = BitmapFactory.decodeByteArray(localImageData.data, 0, localImageData.data.size)
-    return bitmap.asCoilImage()
+    return bitmap.asImage()
 }
